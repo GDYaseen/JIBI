4202e5eb6276eb5386056476781981dd8226138d
Hafsa Ougha <92583179+Hafsaou@users.noreply.github.com>
2024-06-12 13:08:59 +0100

    Update HibernateConfig.java

--- src/main/java/com/jibi/back_end/config/HibernateConfig.java
+++ src/main/java/com/jibi/back_end/config/HibernateConfig.java
@@ -32,9 +32,9 @@ public class HibernateConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/jibi");
+        dataSource.setUrl("jdbc:mysql://roundhouse.proxy.rlwy.net:41052/railway");
         dataSource.setUsername("root");
-        dataSource.setPassword("wrong password");
+        dataSource.setPassword("HRUAqtIeyRaQBFWdszreaVXOdjQETEYz");
         return dataSource;
     }
 
