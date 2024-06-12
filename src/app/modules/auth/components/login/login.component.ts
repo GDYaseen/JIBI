4d9f306f37a424e460eba10f558f8383ea88714f
Amja Do <amri.amjado@gmail.com>
2024-06-12 14:31:48 +0100

    deploy

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -53,7 +53,7 @@ export class LoginComponent implements OnInit, OnDestroy {
   initForm() {
     this.loginForm = this.fb.group({
       email: [
-        '',
+        'hassan@gmail.com',
         Validators.compose([
           Validators.required,
           Validators.email,
@@ -62,7 +62,7 @@ export class LoginComponent implements OnInit, OnDestroy {
         ]),
       ],
       password: [
-        '',
+        'password',
         Validators.compose([
           Validators.required,
           Validators.minLength(8),
